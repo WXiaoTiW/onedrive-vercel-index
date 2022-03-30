eeceb14f4f2e001a69eaa2929647dbc35224ea1c
XiaoTiW <58878080+WXiaoTiW@users.noreply.github.com>
2022-03-30 20:37:39 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,10 +11,10 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/128.png',
+  icon: '/icons/file_1080647.ico',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "Alist",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
